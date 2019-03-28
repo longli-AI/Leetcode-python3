ae4be277a42ac73cb2204632e370255758707afd
longli-ai <>
2019-03-28 19:48:52 +0800

    Update 26_Remove_Duplicates_from_Sorted_Array.py

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -2,9 +2,12 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         for i in range(len(nums)-1, 0, -1):
             if nums[i] == nums[i-1]:
-                nums.pop(i)
-                
+                del nums[i]
+                pass
+            pass
+        pass
+    pass
 '''
-Runtime: 68 ms, faster than 53.40% of Python3 online submissions for Remove Duplicates from Sorted Array.
-Memory Usage: 14.7 MB, less than 5.43% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Runtime: 64 ms, faster than 64.35% of Python3 online submissions for Remove Duplicates from Sorted Array.
+Memory Usage: 14.8 MB, less than 5.43% of Python3 online submissions for Remove Duplicates from Sorted Array.
 '''
