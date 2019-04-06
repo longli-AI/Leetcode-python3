a3d01967481e059da2a3ed79ee23ad1b4dc3d93f
longli-ai <>
2019-04-06 20:47:01 +0800

    Update 020_Valid_Parentheses.py

--- 020_Valid_Parentheses.py
+++ 020_Valid_Parentheses.py
@@ -28,31 +28,3 @@ class Solution:
 Runtime: 64 ms, faster than 9.18% of Python3 online submissions for Valid Parentheses.
 '''
 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        dict1 = {'[':']', '{':'}', '(':')'}
-        list1 = []
-        for i in s:
-            if len(list1) > 0:
-                if list1[-1] in dict1:
-                    if i == dict1[list1[-1]]:
-                        list1.pop()
-                    else:
-                        list1.append(i)
-                        pass
-                    pass
-                else:
-                    list1.append(i)
-                    pass
-                pass    
-            else:
-                list1.append(i)
-                pass
-            pass
-        if len(list1) == 0:
-            return True
-        return False
-'''
-Runtime: 52 ms, faster than 18.42% of Python3 online submissions for Valid Parentheses.
-Memory Usage: 13.1 MB, less than 5.22% of Python3 online submissions for Valid Parentheses.
-'''
