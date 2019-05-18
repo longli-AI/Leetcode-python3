80d595f68ee066769119b6c60dedfb0ddfeb4766
rubust-ai <41643043+rubust-ai@users.noreply.github.com>
2019-05-18 11:02:52 +0800

    Update 167_Two_Sum_II_Input_array_is_sorted.py

--- 167_Two_Sum_II_Input_array_is_sorted.py
+++ 167_Two_Sum_II_Input_array_is_sorted.py
@@ -1,5 +1,25 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l,r = 0,len(numbers)-1
+    
+        while l<=r:
+            if numbers[l] +numbers[r] == target:
+                return [l+1,r+1]
+            if numbers[l] +numbers[r] < target:
+                l += 1
+            if numbers[l] +numbers[r] > target:
+                r -= 1
+        return [l+1,r+1]
+'''
+Runtime: 44 ms, faster than 49.27% of Python3 online submissions for Two Sum II - Input array is sorted.
+Memory Usage: 13.8 MB, less than 5.14% of Python3 online submissions for Two Sum II - Input array is sorted.
+'''
+
+
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
         n = len(numbers)
         l = 0
         r = 0
@@ -17,3 +37,7 @@ class Solution:
 Runtime: 2192 ms, faster than 7.56% of Python3 online submissions for Two Sum II - Input array is sorted.
 Memory Usage: 13.7 MB, less than 5.14% of Python3 online submissions for Two Sum II - Input array is sorted.
 '''
+
+
+
+
