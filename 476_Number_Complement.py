52dcf986dfdd6e53471cc439d6805cf0922d68b8
rubust-ai <41643043+rubust-ai@users.noreply.github.com>
2019-06-09 14:25:34 +0800

    Create 476_Number_Complement.py

---
+++ 476_Number_Complement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        temp = bin(num).replace('0b','')
+        output = ['0b']
+        for i in temp:
+            if i == '0':
+                output.append('1')
+            else:
+                output.append('0')
+        return int(''.join(output),2)
+'''
+Runtime: 36 ms, faster than 84.85% of Python3 online submissions for Number Complement.
+Memory Usage: 13.2 MB, less than 38.90% of Python3 online submissions for Number Complement.
+'''
